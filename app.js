453d901345a3c3eeb0a8fba9cf7997c5ffb00103
Marc Berchtold <me@echolot.io>
2018-11-28 20:41:37 +0100

    Move tms to own folder

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ class App {
         console.log(`Starting node turing machine 🧐`);
         this.parser = new Parser();
 
-        this.parser.loadFile(process.argv[2]).then(file => {
-            const parseRes = this.parser.parseFile(file);
+        if (process.argv[2]) {
+            this.parser.loadFile(process.argv[2]).then(file => {
+                const parseRes = this.parser.parseFile(file);
 
-            this.turingMachine = new TuringMachine(parseRes.tape, parseRes.tapeAlphabet);
-            this.turingMachine.executeCommands(parseRes.commands, process.argv[3] === 'steps');
+                this.turingMachine = new TuringMachine(parseRes.tape, parseRes.tapeAlphabet);
+                this.turingMachine.executeCommands(parseRes.commands, process.argv[3] === 'steps');
 
-            if (process.argv[3] !== 'steps')
-                console.log(`All done... Exiting 🥳`);
-        }).catch(err => console.error(err));
+                if (process.argv[3] !== 'steps')
+                    console.log(`All done... Exiting 🥳`);
+            }).catch(err => console.error(err));
+        }
     }
 }
 
--- parser.js
+++ parser.js
@@ -10,7 +10,7 @@ class Parser {
         console.log(`-- Loading file ${filePath}`);
         const loadFileTimeStart = performance.now();
         return new Promise((resolve, reject) => {
-            fs.readFile(filePath, 'utf-8', (err, data) => {
+            fs.readFile(`./tms/${filePath}`, 'utf-8', (err, data) => {
                 if (err)
                     reject(err);
                 else {
